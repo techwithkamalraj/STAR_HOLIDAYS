36210b93441c4f7314395e860f5a6113545b47a2
Kamalraj David Chitturi <99992423+techwithkamalraj@users.noreply.github.com>
2024-04-03 16:26:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// HAMBURGER ICON
+
+document.addEventListener('DOMContentLoaded', function() {
+    const listIcon = document.querySelector('.bi-list');
+    const sidebar = document.getElementById('sidebar');
+
+    // Toggle sidebar visibility when list icon is clicked
+    listIcon.addEventListener('click', function() {
+        sidebar.classList.toggle('active');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let listIcon = document.querySelector('.bi-list');
+    let closeIcon = document.getElementById('#close-sidebar');
+    let sidebar = document.getElementById('#sidebar');
+
+    // Toggle sidebar visibility when list icon is clicked
+    listIcon.addEventListener('click', function() {
+        sidebar.classList.toggle('active');
+    });
+    
+    closeIcon.addEventListener('click',()=>{
+        sidebar.style.display = 'none';
+    })
+
+});
+
+
+// CHANGING BACKGROUND
+
+// List of image URLs
+const images = [
+    'IMAGES/bg1.jpg',
+    'IMAGES/bg2.jpg',
+    'IMAGES/bg3.jpg'
+];
+
+// Change background image every 5 seconds
+let currentIndex = 0;
+
+function changeBackground() {
+    document.querySelector('.background-image').style.backgroundImage = `url(${images[currentIndex]})`;
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+changeBackground(); // Change background immediately on page load
+
+setInterval(changeBackground, 5000); // Change background every 5 seconds
+
+
+
+
